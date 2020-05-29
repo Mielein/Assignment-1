8d731c60a41a134f82dce41f5cd4501a506f1c77
Mielein <marie.trojan@uni-weimar.de>
2020-05-29 16:27:36 +0200

    storage error

--- source/quicksort.cpp
+++ source/quicksort.cpp
@@ -7,45 +7,45 @@ void swap(int *x, int *y){
     *y = s;
 }
 
-void printArray(int printedArr[]){
-    for(int i = 0; i < sizeof(printedArr); ++i){
-        std::cout<<printedArr[i]<<", "<<std::endl;
-    }
-}
-
 int partition(int a[], int p, int r){
     int x = a[r];
     int i = p-1;
-    for(int j = p; j < r; ++j){
+    for(int j = p; j < r-1; ++j){
         if(a[j] <= x){
             i = i + 1;
             swap(&a[i],&a[j]);
         }
     }
     swap(&a[i + 1], &a[r]);
-    return i+1;
+    return ++i;
 }
 
 void quicksort(int a[],int p,int r){
-    while(p<r){
+    if(p<r){
         int q = partition(a,p,q-1);
         quicksort(a,p,r);
         quicksort(a,q+1,r);
     }
 }
 
+void printArray(int arrPrint[], int len){  
+    for (int i = 0; i < len; i++)  
+        std::cout << arrPrint[i]<<std::endl;  
+}  
+
 int main(){
-    int n;
-    int a[n];
     std::cout<<"Type in the size of your array"<<std::endl;
+    int n;
     std::cin>>n;
-    std::cout<<"Type in numbers that you want in your array and I’ll sort them for you"<<std::endl;
+
+    int a[n];
+    std::cout<<"Type in numbers that you want in your array and I’ll sort them for you "<<std::endl;
     for(int j = 0; j < n; ++j){
-    std::cin>>a[j];    
+        std::cin>>a[j];    
     }
-    quicksort(a, 0, sizeof(a));
-    std::cout<<"Your array sorted by quicksort is: \n"<<std::endl;
-    printArray(a);
+    quicksort(a, 0, n);
+    std::cout<<"Your array sorted by quicksort is: "<<std::endl;
+    printArray(a,n);
     return 0;
 }
 
